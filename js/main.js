9eee4e18cdaa250a1be6b24174c07551454bb1e6
grigoriy-golovin <watson.180491@gmail.com>
2020-05-16 17:03:17 +0300

    сделал task2

---
+++ js/main.js
@@ -0,0 +1,51 @@
+"use strict";
+
+let templateArr = document.querySelectorAll(`template`);
+templateArr = Array.from(templateArr);
+templateArr = templateArr.map((item) => item.content);
+
+const main = document.querySelector(`#main`);
+
+const switchScreens = (index) => {
+  const startLength = main.children.length;
+  for (let i = 0; i < startLength; i++) {
+    main.children[0].remove();
+  }
+
+  const node = templateArr[index].cloneNode(true);
+  main.append(node);
+};
+
+let currentScreen = 0;
+const MAX_SCREEN = 6;
+switchScreens(currentScreen);
+
+const switchNext = () => {
+  currentScreen === MAX_SCREEN ? (currentScreen = 0) : currentScreen++;
+    switchScreens(currentScreen);
+}
+
+const switchPrev = () => {
+  currentScreen === 0 ? (currentScreen = MAX_SCREEN) : currentScreen--;
+    switchScreens(currentScreen);
+}
+
+const keyupHandler = (evt) => {
+  if (evt.keyCode === 39) {
+    switchNext();
+  }
+  if (evt.keyCode === 37) {
+    switchPrev();
+  }
+};
+
+document.addEventListener(`keyup`, keyupHandler);
+
+const arrows = document.querySelector("#arrows").content;
+document.body.append(arrows);
+
+const butPrev = document.querySelector(".arrows__btn--prev");
+const butNext = document.querySelector(".arrows__btn--next");
+
+butPrev.addEventListener(`click`, switchPrev);
+butNext.addEventListener(`click`, switchNext);
